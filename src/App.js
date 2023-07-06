5c7db8c28771810407ab73444c26705bc4dfc7ea
Zali Baker <zali@activeco.com.au>
2023-07-06 15:22:46 +1000

    restart fresh with files from Sandbox

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Header from "./components/Header";
+import "./App.css";
+import Home from "./components/Home";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Home />
+    </div>
+  );
+}
